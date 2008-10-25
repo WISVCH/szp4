7498411c8a2154a503fd929fecdbb91638f3138c
Jeroen Dekkers <jeroen@dekkers.ch>
2008-10-25 02:35:01 +0200

    Small changes on the server

--- cli/init.sh
+++ cli/init.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
-dropdb szp
-createdb szp
+sudo dropdb szp
+sudo createdb szp
 
-/home/jeroen/bzr/szp4/manage.py syncdb --noinput
-/home/jeroen/bzr/szp4/manage.py createsuperuser --username=admin --email=szp@ch.tudelft.nl
+~/szp4/manage.py syncdb --noinput
+~/szp4/manage.py createsuperuser --username=admin --email=szp@ch.tudelft.nl
 
-szp setcontest INITIALIZED 2008-10-04 'test contest' 'test location'
+szp setcontest INITIALIZED 2008-10-24 'Benelux Algorithm Programming Contest 2008' 'Delft'
 
-szp addautojudge 127.0.0.1
+szp addautojudge 131.180.158.61
+szp addautojudge 131.180.158.62
+szp addautojudge 131.180.158.63
+szp addautojudge 131.180.158.64
+szp addautojudge 131.180.158.65
 
-
-szp addcompiler submission.c "gcc -Wall -O2 -g -std=c99 -lm submission.c -o submission" "./submission" .c "GNU C Compiler" 4.3.1
-szp addcompiler submission.cc "g++ -Wall -O2 -g -std=c++98 -lm submission.cc -o submission" "./submission" .cc "GNU C++ Compiler" 4.3.1
+szp addcompiler submission.c "gcc -Wall -O2 -g -std=c99 -lm submission.c -o submission" "./submission" .c "GNU C Compiler" 4.3.2
+szp addcompiler submission.cc "g++ -Wall -O2 -g -std=c++98 -lm submission.cc -o submission" "./submission" .cc "GNU C++ Compiler" 4.3.2
 szp addcompiler 'Problem${LETTER}.java' 'javac -O Problem${LETTER}.java' 'java Problem${LETTER}' .java 'Sun Java Compiler' 1.6.0_07
 
-szp addteamclass 1 testers
+szp addteamclass 1 Contestants
+szp addteamclass 2 Business
+szp addteamclass 3 Spectators
 
-szp addteam testteam1 qa 1 'dw 160' 127.0.0.1 team1 test
-szp addteam testteam2 qa 1 'dw 160' 127.0.0.2 team2 test
+#szp addteam testteam1 qa 1 'dw 160' 127.0.0.1 team1 test
+#szp addteam testteam2 qa 1 'dw 160' 127.0.0.2 team2 test
 
-pushd /home/jeroen/szptest
-szp addproblem A 30 Obfuscate Pink A.in A.out check
-szp addproblem B 30 Epluribus Blue B.in B.out check
-szp addproblem C 30 Coneasoup Yellow C.in C.out check
-popd
+#pushd /home/jeroen/szptest
+#szp addproblem A 30 Obfuscate Pink A.in A.out check
+#szp addproblem B 30 Epluribus Blue B.in B.out check
+#szp addproblem C 30 Coneasoup Yellow C.in C.out check
+#popd
 
-szp addjudge testjudge test
+#szp addjudge testjudge test
 
 #szp setcontest RUNNING
--- settings.py
+++ settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = 'szp'          # Or path to database file if using sqlite3.
-#DATABASE_USER = 'szp'       # Not used with sqlite3.
-DATABASE_USER = ''
-DATABASE_PASSWORD = 'de7Loale'         # Not used with sqlite3.
+DATABASE_USER = 'szp'       # Not used with sqlite3.
+from database import DATABASE_PASSWORD 
+#DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = '127.0.0.1'           # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 

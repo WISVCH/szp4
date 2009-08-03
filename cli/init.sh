522b76a402db5640515c5a6339e12a889fb70141
Mark Janssen <mark@praseodym.net>
2009-08-03 14:24:29 +0200

    Fixed settings.py ignorance, init script and DRYed team/jury status

--- cli/init.sh
+++ cli/init.sh
@@ -2,13 +2,18 @@
 dropdb szp
 createdb szp
 
-/home/jeroen/bzr/szp4/manage.py syncdb --noinput
-/home/jeroen/bzr/szp4/manage.py createsuperuser --username=admin --email=szp@ch.tudelft.nl
+szp() {
+cli/szp.py "$@"
+}
+
+./manage.py syncdb --noinput
+
+echo Creating admin superuser
+./manage.py createsuperuser --username=admin --email=szp@ch.tudelft.nl
 
 szp setcontest INITIALIZED 2008-10-04 'test contest' 'test location'
 
 szp addautojudge 127.0.0.1
-
 
 szp addcompiler submission.c "gcc -Wall -O2 -g -std=c99 -lm submission.c -o submission" "./submission" .c "GNU C Compiler" 4.3.1
 szp addcompiler submission.cc "g++ -Wall -O2 -g -std=c++98 -lm submission.cc -o submission" "./submission" .cc "GNU C++ Compiler" 4.3.1

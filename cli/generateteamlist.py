05b4bacbecd7755d6b039d861308fb478ff73a66
Jeroen Dekkers <jeroen@dekkers.ch>
2010-10-01 23:47:30 +0200

    Use correct column number for spectator flag

--- cli/generateteamlist.py
+++ cli/generateteamlist.py
@@ -11,4 +11,4 @@ cursor.execute ("SELECT * from inschr2010")
 i = 0
 for row in cursor:
     i += 1
-    print "szp addteam -ip team%02d '%s' 'TU Delft' %d 'DW 150.01'" % (i, row[0], row[19]+1)
+    print "szp addteam -ip team%02d '%s' 'TU Delft' %d 'DW 150.01'" % (i, row[0], row[18]+1)
